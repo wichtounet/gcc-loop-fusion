9667cf7ff38d20cd7b0d6be1e4bdcb3221e65f20
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-10-12 23:59:53 +0200

    Comments on the current state

--- loop_fusion_c.cpp
+++ loop_fusion_c.cpp
@@ -2,6 +2,13 @@
 
 static const unsigned long SIZE = 100000000;
 
+/*
+ * Test to verify the fusion of more than two loops
+ * 
+ * Currently, only two loops are fused, because the distance between the next ones is too high,
+ * some more cleaning will be necessary before it can be merged again.
+ */
+
 int main(){
     unsigned long* array_1 = new unsigned long[SIZE];
     unsigned long* array_2 = new unsigned long[SIZE];
